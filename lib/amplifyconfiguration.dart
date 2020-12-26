63b7c9fd332c02c52af40aaa182146fee7bea887
Adilah <adilah.aay@gmail.com>
2020-12-27 01:38:53 +0800

    first commit

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:fa70c89c-dd87-493f-9dc5-b5de6d0d9ae3",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_xpkeAOK55",
+                        "AppClientId": "4d167r072enfuh67jsp8q2t59u",
+                        "AppClientSecret": "1lpjskq0ovjgrhlpshtp805ce7qmko71v3ncc70q20omhd1bek5g",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
